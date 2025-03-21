bf6e5871a6195c9361410e7a197ea9d24e75bd16
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-03-20 22:27:37 -0300

    feat: create 'sign in' page (code and css)

---
+++ app/signin/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import styles from "../signin/Signin.module.css";
+
+export default function Signin() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.input}>
+            <h1>Welcome back! 🌟💋🤠</h1>
+            <p>Enter your details below.</p>
+                    <input type="email" placeholder="💌 email" />
+                    <input type="password" placeholder="🔒 password" />
+                    <button className={styles.button}>SIGN IN</button>
+                    <p className={styles.link}>New here? No worries, <a href="/signup"> sign up</a> and let’s get you started!</p>
+            </div>
+        </div>
+    );
+}
